880a2a4f8850d2aa2cfd8735f0e6b8db36f3fb44
Simon Gravelle <simon.gravelle@live.fr>
2024-06-24 22:55:31 +0200

    test

--- functions.py
+++ functions.py
@@ -26,12 +26,10 @@ def prepare_figure(mode, transparency = False, desired_figsize=(18,6), serif=Tru
             "font.serif": ["Palatino"],
         }) 
     else:
-        plt.rcParams.update({
-            "text.usetex": True,
-            'text.latex.preamble': [r'\usepackage{cmbright}', r'\usepackage{amsmath}'],
-            #"font.family": "sans-serif",
-            #"font.sans-serif": ["Open Sans"],
-        })              
+        plt.rcParams['text.usetex'] = True 
+        plt.rcParams['text.latex.preamble'] = [r'\usepackage[cm]{sfmath}']
+        plt.rcParams['font.family'] = 'sans-serif'
+        plt.rcParams['font.sans-serif'] = 'cm'
     return fig
 
 def add_subplotlabels(fig, ax, labels, shift=0.2, specific_shift=None, color=None):
