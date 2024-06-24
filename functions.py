87722fa0ccf38354594886d2280167f126f9d71a
Simon Gravelle <simon.gravelle@live.fr>
2024-06-24 22:35:19 +0200

    test

--- functions.py
+++ functions.py
@@ -29,7 +29,7 @@ def prepare_figure(mode, transparency = False, desired_figsize=(18,6), serif=Tru
         plt.rcParams.update({
             "text.usetex": True,
             "font.family": "sans-serif",
-            "font.serif": ["Palatino"],
+            "font.serif": ["Open Sans"],
         })              
     return fig
 
